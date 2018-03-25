50d7e9bebcaacc1857ba955842ac819da788a624
Erik Martines Sanches <erik.martinessanches@gmail.com>
2018-03-25 16:54:37 +0200

    Initial commit.

---
+++ PartyInvites/Models/IRepository.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+namespace PartyInvites.Models {
+    public interface IRepository {
+        IEnumerable<GuestResponse> Responses {get; }
+        void AddResponse(GuestResponse response);
+    }
+}
